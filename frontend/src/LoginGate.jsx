41d117cbcd56998d0fb6dfc498949fcfc2eb29ad
Kareem <ksaifeldawlla@gmail.com>
2026-01-08 07:28:23 -0500

    Barebones login page done

--- frontend/src/LoginGate.jsx
+++ frontend/src/LoginGate.jsx
@@ -7,14 +7,22 @@ function LoginGate({ onAuth }) {
 
         onAuth('github');
     };
+    const loginLocal =() => {
+        onAuth('local');
+    }
 
     return (
         <div className="login-screen">
             <TitleBar />
-            <div className="main-content">
-                <div className="left-panel">
-<button onClick={loginWithGithub}>
+            <div className="login-container">
+                <div className="center-panel">
+                    <p className="Title">FlowState</p>
+                    <p className="Subtext">Slogan go here!</p>
+            <button className="Github-button" onClick={loginWithGithub}>
                 Sign in with GitHub
+            </button>
+            <button className="Local-button" onClick={loginLocal}>
+                Sign in as a Local User
             </button>
             </div>
             
